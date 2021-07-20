e6121cb5b8f8c96148cc44326459d44aecbb5c74
Hyungjun Lim <junlim@hyungjuimacmini.lan>
2021-07-20 08:25:39 -0400

    use the relative price

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def handle_record_prediction():
     move_type = request.args.get('move_type')
     symbol = request.args.get('symbol')
     market = request.args.get('market')
+    price = request.args.get('price')
     epoch_seconds = int(request.args.get('epoch_seconds'))
     max_jump_backward_percent = float(request.args.get('max_jump_backward_percent'))
     min_drop_backward_percent = float(request.args.get('min_drop_backward_percent'))
@@ -27,13 +28,13 @@ def handle_record_prediction():
     daily_stat_parameters = daily_stat.get_stat_prameters(symbol, market, epoch_seconds, windowed_min, windowed_max, windowed_avg)
     p = prediction.predict(
         min_drop_backward_percent, max_jump_backward_percent,
-        daily_stat_parameters.min_sofar_today,
-        daily_stat_parameters.max_sofar_today,
-        daily_stat_parameters.avg_sofar_today,
-        daily_stat_parameters.prev_daily_min,
-        daily_stat_parameters.prev_daily_max,
-        daily_stat_parameters.prev_daily_avg,
-        daily_stat_parameters.prev_day_close
+        daily_stat_parameters.min_sofar_today / price,
+        daily_stat_parameters.max_sofar_today / price,
+        daily_stat_parameters.avg_sofar_today / price,
+        daily_stat_parameters.prev_daily_min / price,
+        daily_stat_parameters.prev_daily_max / price,
+        daily_stat_parameters.prev_daily_avg / price,
+        daily_stat_parameters.prev_day_close / price
     )
 
     record.record_prediction(symbol, market, epoch_seconds, move_type, 'jump', p)
